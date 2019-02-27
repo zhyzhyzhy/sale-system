8cbe965abf028aa2c0e8cfb61fe144fa85dd6e38
zhyzhyzhy <1012146386@qq.com>
2019-02-27 23:24:23 +0800

    增加public页面

--- src/main/java/cc/lovezhy/netease/sale/model/SellerGoodModel.java
+++ src/main/java/cc/lovezhy/netease/sale/model/SellerGoodModel.java
@@ -1,4 +1,19 @@
 package cc.lovezhy.netease.sale.model;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class SellerGoodModel {
+
+    private Integer id;
+
+    private String title;
+
+    private String image;
+
+    private BigDecimal price;
+
+    private Boolean hasBeenSold;
 }
