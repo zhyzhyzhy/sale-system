d87fb3cf0e318592c37971c99d2b0bd6d1004a31
zhyzhyzhy <1012146386@qq.com>
2019-02-27 20:54:47 +0800

    增加public页面

--- src/main/java/cc/lovezhy/netease/sale/model/BuyingGoodModel.java
+++ src/main/java/cc/lovezhy/netease/sale/model/BuyingGoodModel.java
@@ -1,4 +1,9 @@
 package cc.lovezhy.netease.sale.model;
 
+import lombok.Data;
+
+@Data
 public class BuyingGoodModel {
+    private Integer id;
+    private Integer number;
 }
--- src/main/java/cc/lovezhy/netease/sale/web/SettleAccountController.java
+++ src/main/java/cc/lovezhy/netease/sale/web/SettleAccountController.java
@@ -1,4 +1,22 @@
 package cc.lovezhy.netease.sale.web;
 
+import cc.lovezhy.netease.sale.common.UserInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SettleAccountController {
+
+    @GetMapping("/settleAccount")
+    public String settleAccountPage() {
+        return "settleAccount";
+    }
+
+    @GetMapping(path = {"/account.html", "/account"})
+    public String account(UserInfo userInfo, Model model) {
+        model.addAttribute("userInfo", userInfo);
+        return "account";
+    }
+
 }
